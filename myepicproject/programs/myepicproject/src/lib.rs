53811e29adaf18a9cc207dca7178d95bd461a22e
polea003 <polea003@fiu.edu>
2022-02-05 05:55:27 -0500

    GIF counter on solana blockchain

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -4,11 +4,40 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod myepicproject {
-    use super::*;
-    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
-        Ok(())
-    }
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+  
+	// Another function woo!
+  pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+    // Get a reference to the account and increment total_gifs.
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
